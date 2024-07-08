9b8ff2c797aefb9cae2d13d3f833841140ee2d43
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-08 13:30:47 -0600

    Country table component

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/country';
 
 @Component({
-  selector: 'countries-country-page',
+  selector: 'countries-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
 export class CountryPageComponent {
+
 
 }
