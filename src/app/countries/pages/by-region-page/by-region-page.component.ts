813d2f0a5eb1b65beaf2e2697468fc9443c3f7e1
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-09 09:28:13 -0600

    Button search for Regions

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania'| '';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,15 +13,18 @@ export class ByRegionPageComponent {
   constructor(private countriesService: CountriesService){}
 
   public countries: Country[] = [];
-  public isLoading: boolean = false;
 
-  searchByRegion(region: string){
-    this.isLoading = true;
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+
+  searchByRegion(region: Region){
+
+    this.selectedRegion = region;
 
     this.countriesService.SearchRegion(region)
       .subscribe(country => {
         this.countries = country
-        this.isLoading = false;
+
       })
   }
 }
